c175d115b2ad29621596d8aeb3ffa8bbda3b7c49
Xyphos <TheGreatXyphos@gmail.com>
2025-08-08 03:29:39 -0700

    dev

--- index.js
+++ index.js
@@ -68,6 +68,8 @@ function ajaxError(xhr, status, message, clue) {
 // ================================================================================================
 
 function jsonLocales(json) {
+	console.log(`\n\nlocales.json\n\n{$json}`);
+	
 	json.supported.forEach((lang) => {
 		
 		let locale = `locale-${lang}`;
